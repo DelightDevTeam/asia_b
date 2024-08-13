9d2332eedb02b4d7d10ffc5e92d63704a7d432dd
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-08-13 17:26:40 +0700

    a

--- src/pages/Promotion.jsx
+++ src/pages/Promotion.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
+import useFetch from '../hooks/useFetch'
+import BASE_URL from '../hooks/baseURL'
 
 const Promotion = () => {
-    const imgs=[
-        'https://tbbslot3d.netlify.app/assets/promotion1-D1y5gQGM.png',
-        'https://tbbslot3d.netlify.app/assets/promotion2-B5iW7-Mx.gif',
-        'https://tbbslot3d.netlify.app/assets/promotion4-D8z2ql8D.gif'
-    ]
+  const {data: promos} = useFetch(BASE_URL + '/promotion');
+  
   return (
       <div className='py-4 px-3 px-sm-5 mx-lg-5 mb-5'>
         <h1 className="fw-bold text-center">Promotion</h1>
-        {imgs.map((img,index)=>{
-          return  <img key={index} src={img} className='my-3 my-sm-4 promotionImg' />
+        {promos && promos.map((promo,index)=>{
+          return  <img key={index} src={promo?.img_url} className='my-3 my-sm-4 promotionImg' />
         })}
      </div>
   )
